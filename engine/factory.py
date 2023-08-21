d3afac90cd69d7da0aeb86a08ae57484108e8eef
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2023-08-21 18:26:49 -0400

    RFR: refactor combat, part 1 - discrete functions for every step

--- engine/factory.py
+++ engine/factory.py
@@ -6,8 +6,10 @@ def iron_sword():
     return Weapon(
         name='Iron Sword',
         might=5,
+        max_durability=46,
         hit=95,
         crit=0,
         weight=5,
         rng=(1, 1),
+        cur_durability=46,
     )
