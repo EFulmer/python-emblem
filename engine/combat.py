d657f3bf3b5bc7dde63bd03e193387278d66cf9f
Eric Fulmer <2952996+EFulmer@users.noreply.github.com>
2023-08-22 20:36:40 -0400

    ENH: add first draft of Arena type battling

--- engine/combat.py
+++ engine/combat.py
@@ -1,4 +1,5 @@
 from dataclasses import replace
+import logging
 import random
 from typing import Tuple
 
@@ -8,37 +9,13 @@ from engine.types import Character, Weapon
 def rng():
     return random.randint(1, 100)
 
-
-# TODO: make the combat function into a Link Arena-style generator:
-# alternate until one unit loses
-def combat(attacker: Character, defender: Character):
-    """Do one round of combat. Use the first weapon in each character's
-    inventory.
-    """
-    attacker_weapon = attacker.inventory[0]  # TODO check that this is a weapon. maybe in another fn?
-    defender_weapon = defender.inventory[0]  # TODO as above
-    #  TODO range check
-    attacker_power = attacker.pwr + attacker_weapon.might
-    #  TODO defense or resistance
-    attacker_dmg = attacker_power - defender.dfn
-    defender.cur_hp = min(attacker_dmg - defender.cur_hp, 0)
-    # TODO probably a better way to do this
-    if defender.cur_hp == 0:
-        return
-    defender_power = defender.pwr = defender_weapon.might
-    # TODO defense or resistance
-    defender_dmg = defender_power - attacker.dfn
-    attacker.cur_hp = min(defender_dmg - attacker.cur_hp, 0)
-    if attacker.cur_hp == 0:
-        return
-
 def hit_rate(attacker: Character, defender: Character) -> int:
     """Return hit rate, which should be between 1 and 100.
     """
     # TODO weapon triangle
     accuracy = attacker.inventory[0].hit + attacker.skl * 2 + attacker.lck // 2
     avoid = defender.avoid
-    return accuracy - avoid
+    return min(100, max(accuracy - avoid, 0))
 
 
 def attack_hits(attacker: Character, defender: Character) -> bool:
@@ -59,6 +36,29 @@ def attack(attacker: Character, defender: Character) -> Tuple[Character, Charact
     """
     if attack_hits(attacker=attacker, defender=defender):  # TODO critical hits
         attack_power = attacker.inventory[0].might + attacker.pwr - defender.dfn  # TODO res
-        new_defender_hp = min(attack_power - defender.dfn, 0)
+        new_defender_hp = max(defender.cur_hp - attack_power - defender.dfn, 0)
         # TODO durability
         return (attacker, replace(defender, cur_hp=new_defender_hp))
+    else:
+        return (attacker, defender)
+
+
+def combat_summary(c1: Character, c2: Character):
+    pass
+
+
+def arena(c1: Character, c2: Character):
+    """Do 'arena' style combat between the two characters, until one's
+    HP hits 0.
+
+    Args:
+        c1: The first character to attack.
+        c2: The second character to attack.
+    """
+    while c1.cur_hp > 0 or c2.cur_hp > 0:
+        logging.info(f"{c1.name} attacks!")
+        c1, c2 = attack(c1, c2)
+        logging.info(f"{c2.cur_hp = }")
+        logging.info(f"{c2.name} attacks!")
+        c2, c1 = attack(c2, c1)
+        logging.info(f"{c1.cur_hp = }")
